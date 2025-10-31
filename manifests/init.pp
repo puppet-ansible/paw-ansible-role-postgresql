cd79a4305b36722bba79dc6d8284bc23c7da2e5f
Chris Allen <chris.j.allen1979@gmail.com>
2025-10-31 12:54:46 +0000

    Automated conversion from geerlingguy.postgresql Ansible role

--- manifests/init.pp
+++ manifests/init.pp
@@ -61,7 +61,7 @@ class paw_ansible_role_postgresql (
   Optional[Boolean] $par_logoutput = undef,
   Optional[Boolean] $par_exclusive = undef
 ) {
-  # Execute the Ansible role using PAR (Puppet Ansible Runner)
+# Execute the Ansible role using PAR (Puppet Ansible Runner)
   $vardir = pick($facts['puppet_vardir'], $settings::vardir, '/opt/puppetlabs/puppet/cache')
   $playbook_path = "${vardir}/lib/puppet_x/ansible_modules/ansible_role_postgresql/playbook.yml"
 
